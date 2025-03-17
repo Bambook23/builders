1fe9ccb0d1fb0ef3fc7c9fe624a81b0802d56e7d
Bambook23 <bambook251@gmail.com>
2025-03-17 20:37:34 +0000

    write gradle configuration

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,5 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  plugins: [
+   new HtmlWebpackPlugin({
+     template: './src/index.html',
+        }),
+   new CopyPlugin({
+      patterns: [
+        { from: "./src/style.css", to: "css/" },
+      ],
+    }),
+  ],
 };
